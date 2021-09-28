647990b72ee6d9b020fed1a99ac2e7d6384374fc
anidiotonrampage <84858134+anidiotonrampage@users.noreply.github.com>
2021-09-28 15:11:07 +0700

    Add Staticman

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,15 @@
 module.exports = function(eleventyConfig) {
+    
+    
+    staticman_url: https://eleventy-comment.herokuapp.com/v2/entry/anidiotonrampage/Wedding-Invitation/eleventy/comments
+
+
+    eleventyConfig.addPassthroughCopy("src");
+
     return {
         dir: {
             input: "src",
             output: "public"
         }
-    }
+    };
 }
